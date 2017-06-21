a8f13fa66c62ce2ac2d0f70521b7603dc936053e
Aboli Dole <aboli.dole@gmail.com>
2017-06-21 14:28:53 -0400

    Completed large-hero and intro-content block

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+
+
+
+
+// gulp.task('default',function(){
+//   console.log("Yay gulp task created!");
+// });
+//
+// gulp.task('html',function(){
+//   console.log("html here!");
+// });
